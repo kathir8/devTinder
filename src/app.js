617282769fea7c738739324cd3020eb022bc3a45
Kathiravan <sukn99@gmail.com>
2026-03-05 09:24:01 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -5,6 +5,36 @@ const port = 3000;
 
 // Request handler
 
+//app.use("/route", rH, [rH2, rH3], rH4, rh5);
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+  },
+
+  (req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
+  }
+);
+
 
 app.get("/user/:userId",(req,res) => {
     console.log(req.params);
