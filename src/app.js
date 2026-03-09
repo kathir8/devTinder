7025b5730ca057612b17c62ec0bd700fedffb8ee
Kathiravan <sukn99@gmail.com>
2026-03-09 08:58:46 +0530

    CRUD - Users

--- src/app.js
+++ src/app.js
@@ -4,16 +4,11 @@ const app = express();
 const port = 3000;
 const User = require('./models/user');
 
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
 
-  const user = new User({
-    firstName: 'Akshay',
-    lastName: 'Saini',
-    emailId: 'akshay@gmail.com',
-    password: 'akshay@123'
-  });
-
+  const user = new User(req.body);
   try {
     await user.save();
     res.send('User saved successfully');
@@ -21,8 +16,61 @@ app.post('/signup', async (req, res) => {
     res.status(400).send('Error saving user: ' + err.message);
   }
 
-})
+});
 
+// Get user by email
+app.get('/user', async (req, res) => {
+  const userEmail = req.body.email;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      return res.status(404).send('User not found');
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send('Error fetching user: ' + err.message);
+  }
+
+});
+
+
+// Get all users
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      return res.status(404).send('User not found');
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send('Error fetching user: ' + err.message);
+  }
+});
+
+// Delete user by ID
+app.delete("/user", async (req, res) => {
+ const userId = req.body.userId;
+   try {
+    const users = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send('Error deleting user: ' + err.message);
+  }
+});
+
+// Update user by ID
+app.patch("/user", async(req,res)=>{
+  const data = req.body;
+  const userId = data.userId;
+  try {
+    const users = await User.findByIdAndUpdate(userId, data);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send('Error updating user: ' + err.message);
+  }
+});
 
 
 connectDB()
@@ -35,12 +83,14 @@ connectDB()
     console.error('Database connection failed:', err);
   });
 
+
 app.use("/", (err, req, res) => {
   if (err) {
     res.status(500).send("Wild card handling error!!");
   }
   console.log('use will matches all http (get, post, delete, put) request');
 });
+
 
 app.listen(port, () => {
   console.log('Server is running on ' + 'http://localhost:' + port);
