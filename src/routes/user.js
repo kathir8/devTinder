adafac8e431507651042b9835592ae443587b48d
kathiravan <kathiravans@boodskap.io>
2026-03-15 20:39:25 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require('express');
 const userRouter = express.Router();
 
 const { userAuth } = require('../middlewares/auth');
-const connectionRequest = require('../models/connectionRequest');
+const ConnectionRequest = require('../models/connectionRequest');
+const User = require('../models/user');
 
 const USER_SAFE_DATA = 'firstName lastName age gender photoUrl about skills';
 
@@ -11,7 +12,7 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
 
     try {
         const loggedInUser = req.user;
-        const data = await connectionRequest.find({
+        const data = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: 'interested'
         }).populate('fromUserId', USER_SAFE_DATA);
@@ -30,7 +31,7 @@ userRouter.get('/user/requests/received', userAuth, async (req, res) => {
 userRouter.get('/user/connections', userAuth, async (req, res) => {
     try {
         const loggedInUserId = req.user._id;
-        const connectionRequests = await connectionRequest.find({
+        const connectionRequests = await ConnectionRequest.find({
             $or: [
                 { toUserId: loggedInUserId, status: 'accepted' },
                 { fromUserId: loggedInUserId, status: 'accepted' },
@@ -52,6 +53,45 @@ userRouter.get('/user/connections', userAuth, async (req, res) => {
     } catch (err) {
         return res.status(400).send("ERROR: " + err.message);
     }
+});
+
+userRouter.get('/user/feed', userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        // Get all users who have connection requests with the logged-in user
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select('fromUserId toUserId');
+
+        // Extract the user IDs that are already connected/requested
+        const connectedUserIds = new Set();
+        connectionRequests.forEach(req => {
+            connectedUserIds.add(req.fromUserId.toString());
+            connectedUserIds.add(req.toUserId.toString());
+        });
+
+        // Get all users except the logged-in user
+        const users = await User.find({
+            $and: [
+                { _id: { $ne: loggedInUser._id } },
+                { _id: { $nin: Array.from(connectedUserIds) } }
+            ]
+        }).select(USER_SAFE_DATA);
+
+        // Filter out users who already have connection requests
+        if (!users.length) {
+            return res.status(404).send('User not found');
+        }
+
+        res.send(users);
+    } catch (err) {
+        return res.status(400).send("ERROR: " + err.message);
+    }
+
 });
 
 
