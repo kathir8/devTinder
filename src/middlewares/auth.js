04848998ed0e6f9361a889c4911670f6f1874584
Kathiravan <sukn99@gmail.com>
2026-03-16 09:18:31 +0530

    error msg change

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,7 +7,7 @@ const userAuth = async (req, res, next) => {
         const { token } = req.cookies;
         
         if (!token) {
-            throw new Error("Authentication token is not valid");
+            return res.status(401).send("Please Login!");
         }
 
         const decodedobj = jwt.verify(token, SECRET_KEY);
