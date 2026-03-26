7e3f3d48b328305920f04df6cf6380dbcc99a34b
kathiravan <kathiravans@boodskap.io>
2026-03-26 05:34:10 +0530

    unwanted code remove

--- src/app.js
+++ src/app.js
@@ -26,62 +26,6 @@ app.use('/', requestRouter);
 app.use('/', userRouter);
 
 
-
-// Get all users
-app.get('/feed', async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length === 0) {
-      return res.status(404).send('User not found');
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.status(400).send('Error fetching user: ' + err.message);
-  }
-});
-
-// Delete user by ID
-app.delete("/user", async (req, res) => {
-  const userId = req.body.userId;
-  try {
-    await User.findByIdAndDelete(userId);
-    res.send("User deleted successfully");
-  } catch (err) {
-    res.status(400).send('Error deleting user: ' + err.message);
-  }
-});
-
-// Update user by ID
-app.patch("/user/:userId", async (req, res) => {
-  const data = req.body;
-  const userId = req.params?.userId;
-  try {
-    const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "age", "gender"];
-    const isUpdateAllowed = Object.keys(data).every(v => ALLOWED_UPDATES.includes(v));
-
-    if (!isUpdateAllowed) {
-      throw new Error("Update not allowed!!");
-    }
-
-    await User.findByIdAndUpdate(userId, data, { runValidators: true });
-    res.send("User updated successfully");
-  } catch (err) {
-
-    res.status(400).send('Error updating user: ' + err.message);
-  }
-});
-
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("Wild card handling error!!");
-  }
-  console.log('use will matches all http (get, post, delete, put) request');
-});
-
-
-
 connectDB()
   .then(() => {
     console.log('Database connected successfully');
